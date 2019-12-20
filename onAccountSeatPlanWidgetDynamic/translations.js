4809e340e70c20d3b19c52ccd128e4611b43de43
Rabie Sulman <rsulman@encoretickets.co.uk>
2019-12-20 16:53:25 +0000

    add translations

---
+++ onAccountSeatPlanWidgetDynamic/translations.js
@@ -0,0 +1,261 @@
+var translations = `
+{
+    "seatList": {
+       "findingAvailability": {
+          "en-US": "[T] Finding Availability",
+          "sv_SE": "Hitta tillgänglighet"
+       },
+       "reservingSelection": {
+          "en-US": "[T] Reserving Selection",
+          "sv_SE": "Boka val"
+       },
+       "chooseYourArea": {
+          "en-US": "[T] Choose your area",
+          "sv_SE": "Välj ditt område"
+       },
+       "chooseYourSeats": {
+          "en-US": "[T] Choose your seats",
+          "sv_SE": "Välj dina platser"
+       },
+       "filterBy": {
+          "en-US": "[T] Filter by:",
+          "sv_SE": "Filtrera efter"
+       },
+       "pricesDisplayed": {
+          "en-US": "[T] Prices displayed are per ticket",
+          "sv_SE": "Visade priser är per biljett"
+       },
+       "seatAllocationCopy": {
+          "en-US": "[T] Your seats will be together with unrestricted views of the stage",
+          "sv_SE": "Dina platser kommer att vara tillsammans med obegränsad utsikt över scenen"
+       },
+       "noBookingFee": {
+          "en-US": "[T] Nessuna commissione di prenotazione",
+          "sv_SE": "Ingen bokningsprovision"
+       },
+       "restrictions": {
+          "restrictedView": {
+             "en-US": "[T] These seats have restricted view",
+             "sv_SE": "Dessa platser har begränsad utsikt"
+          },
+          "sideView": {
+             "en-US": "[T] These seats are side view",
+             "sv_SE": "Dessa säten är sidovy"
+          }
+       },
+       "viewMore": {
+          "en-US": "[T] + View more",
+          "sv_SE": "+ Visa mer"
+       }
+    },
+    "seatSummary": {
+       "seats": {
+          "en-US": "[T] Seats",
+          "sv_SE": "Säten"
+       },
+       "restrictedView": {
+          "en-US": "[T] Restricted view",
+          "sv_SE": "Begränsad utsikt"
+       },
+       "sideView": {
+          "en-US": "[T] Side view",
+          "sv_SE": "Sidovy"
+       },
+       "seatPrice": {
+          "en-US": "[T] Seat price",
+          "sv_SE": "Sitspris"
+       },
+       "noBookingFee": {
+          "en-US": "[T] No booking fee",
+          "sv_SE": "Ingen bokningsavgift"
+       },
+       "totalPrice": {
+          "en-US": "[T] Total price",
+          "sv_SE": "Totalbelopp"
+       },
+       "addToBasket": {
+          "en-US": "[T] Add to basket",
+          "sv_SE": "Lägg i korgen"
+       }
+    },
+    "seatDetails": {
+       "textForLink": {
+          "en-US": "[T] Important seat info",
+          "sv_SE": "Viktig platsinformation"
+       }
+    },
+    "seatPlanThumbnail": {
+       "linkText": {
+          "en-US": "[T] View venue seat plan",
+          "sv_SE": "Visa platsplats"
+       },
+       "imageTitle": {
+          "en-US": "[T] Venue seating plan",
+          "sv_SE": "Platsplatsplan"
+       }
+    },
+    "legend": {
+       "restrictedView": {
+          "en-US": "[T] Restricted view",
+          "sv_SE": "Begränsad utsikt"
+       },
+       "offer": {
+          "en-US": "[T] Offer",
+          "sv_SE": "Erbjudande"
+       },
+       "restrictedViewOffer": {
+          "en-US": "[T] Restricted view & offer",
+          "sv_SE": "Begränsad utsikt & Erbjudande"
+       }
+    },
+    "seatPlan": {
+       "notAvailable": {
+          "en-US": "[T] notAvailable",
+          "sv_SE": "inte tillgänglig"
+       },
+       "sectionAvailability.none": {
+          "en-US": "[T] sectionAvailability.none",
+          "sv_SE": ""
+       },
+       "selected": {
+          "en-US": "[T] selected",
+          "sv_SE": "vald"
+       },
+       "noOrphanSeats": {
+          "en-US": "[T] noOrphanSeats",
+          "sv_SE": "[T] noOrphanSeats"
+       },
+       "maxSelectionReached": {
+          "en-US": "[T] maxSelectionReached",
+          "sv_SE": "[T] maxSelectionReached"
+       },
+       "deselectOthersFirst": {
+          "en-US": "[T] deselectOthersFirst",
+          "sv_SE": "[T] deselectOthersFirst"
+       },
+       "available.places": {
+          "en-US": "[T] available.places",
+          "sv_SE": "[T] available.places"
+       },
+       "available.seats": {
+          "en-US": "[T] available.seats",
+          "sv_SE": "posti disponibili"
+       },
+       "choosePriceLevel": {
+          "en-US": "[T] choosePriceLevel",
+          "sv_SE": "[T] choosePriceLevel"
+       },
+       "choosePriceLevels": {
+          "en-US": "[T] choosePriceLevels",
+          "sv_SE": "[T] choosePriceLevels"
+       },
+       "singleTicketAvailableInSection": {
+          "en-US": "[T] singleTicketAvailableInSection",
+          "sv_SE": "[T] singleTicketAvailableInSection"
+       },
+       "multipleTicketsAvailableInSection": {
+          "en-US": "[T] multipleTicketsAvailableInSection",
+          "sv_SE": "[T] multipleTicketsAvailableInSection"
+       },
+       "ticketsAvailableFrom": {
+          "en-US": "[T] ticketsAvailableFrom",
+          "sv_SE": "[T] ticketsAvailableFrom"
+       },
+       "selectQuantity": {
+          "en-US": "[T] selectQuantity",
+          "sv_SE": "[T] selectQuantity"
+       },
+       "close": {
+          "en-US": "[T] close",
+          "sv_SE": "[T] close"
+       },
+       "renderingFailed": {
+          "en-US": "[T] renderingFailed",
+          "sv_SE": "[T] renderingFailed"
+       },
+       "renderingFailedTryAgain": {
+          "en-US": "[T] renderingFailedTryAgain",
+          "sv_SE": "[T] renderingFailedTryAgain"
+       },
+       "clickToSelect": {
+          "en-US": "[T] clickToSelect",
+          "sv_SE": "Clic per selezionare"
+       },
+       "clickToDeselect": {
+          "en-US": "[T] clickToDeselect",
+          "sv_SE": "Klick per deselezionare"
+       },
+       "row": {
+          "en-US": "[T] row",
+          "sv_SE": "rad"
+       },
+       "seat": {
+          "en-US": "[T] seat",
+          "sv_SE": "sittplats"
+       },
+       "table": {
+          "en-US": "[T] table",
+          "sv_SE": "tabell"
+       },
+       "section": {
+          "en-US": "[T] section",
+          "sv_SE": "sektion"
+       },
+       "restrictedView": {
+          "en-US": "[T] restrictedView",
+          "sv_SE": "Begränsad vision"
+       },
+       "accessible": {
+          "en-US": "[T] accessible",
+          "sv_SE": "[T] tillgänglig"
+       },
+       "companionSeat": {
+          "en-US": "[T] companionSeat",
+          "sv_SE": "[T] companionSeat"
+       },
+       "sessionExpired": {
+          "en-US": "[T] sessionExpired",
+          "sv_SE": "[T] sessionExpired"
+       },
+       "sessionExpiredAllPlacesReleased": {
+          "en-US": "[T] sessionExpiredAllPlacesReleased",
+          "sv_SE": "[T] sessionExpiredAllPlacesReleased"
+       },
+       "sessionExpiredStartOver": {
+          "en-US": "[T] sessionExpiredStartOver",
+          "sv_SE": "[T] sessionExpiredStartOver"
+       },
+       "useMetaKeyToZoom": {
+          "en-US": "[T] useMetaKeyToZoom",
+          "sv_SE": "Använd% a + bläddra för att zooma"
+       },
+       "couch": {
+          "en-US": "[T] Couch",
+          "sv_SE": "Soffa"
+       },
+       "bar": {
+          "en-US": "[T] Bar",
+          "sv_SE": "Bar"
+       },
+       "select": {
+          "en-US": "[T] Select",
+          "sv_SE": "Välj"
+       },
+       "deselect": {
+          "en-US": "[T] Deselect",
+          "sv_SE": "Välja bort"
+       },
+       "noLongerAvailable": {
+          "en-US": "[T] No longer available",
+          "sv_SE": "Inte längre tillgänglig"
+       },
+       "pickCategory": {
+          "en-US": "[T] Choose a category",
+          "sv_SE": "Välj en kategori"
+       },
+       "filterCategories": {
+          "en-US": "[T] Filter categories",
+          "sv_SE": "Filtrera kategorier"
+       }
+    }
+ }`;
